ee7ce057a4633be31fa70a7ea7716bf66949a7c0
Tomasz <falinskit@gmail.com>
2025-11-28 09:54:31 +0100

    add api key

--- src/app/service/cards.service.ts
+++ src/app/service/cards.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Observable} from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { EMPTY, expand, map, Observable, reduce} from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Pullcard } from '../components/module/pullcard';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,9 @@ export class CardsService {
   private cardsUrl = 'https://api.pokemontcg.io/v2/cards/';
 
   getCards(page: number): Observable<Pullcard> {
-    return this.http.get<Pullcard>(`${this.cardsUrl}?page=${page}`);
+    const header = { 'X-Api-Key': environment.apiKey };
+
+    return this.http.get<Pullcard>(`${this.cardsUrl}?pageSize=10` , { headers: header });
   }
 
   getCardDetail(idCard: string): Observable<Pullcard> {
@@ -20,5 +23,22 @@ export class CardsService {
   }
   getSimilarCards(types): Observable<any> {
     return this.http.get<Pullcard>(`${this.cardsUrl}?q=types:${types}`);
+  }
+
+
+  public getCardsPage(page = 1, pageSize = 250): Observable<Pullcard> {
+    const params = new HttpParams()
+      .set('page', String(page))
+      .set('pageSize', String(pageSize));
+    return this.http.get<Pullcard>(`${this.cardsUrl}`, { params });
+  }
+
+  public getAllCards(): Observable<Pullcard[]> {
+    const pageSize = 250;
+    return this.getCardsPage(1, pageSize).pipe(
+      expand(res => (res.page * res.pageSize < res.count ? this.getCardsPage(res.page + 1, pageSize) : EMPTY)),
+      map(res => res.data),
+      reduce((all, pageData) => all.concat(pageData), [])
+    );
   }
 }
